2573be15b840270fdc9b5a8702230c30d25374cc
Misaf <misaf.1990@gmail.com>
2026-06-14 13:16:46 +0330

    Use UseModel attribute in tenant domain factory

--- database/factories/TenantDomainFactory.php
+++ database/factories/TenantDomainFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Misaf\VendraTenant\Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\Attributes\UseModel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Misaf\VendraTenant\Models\Tenant;
@@ -12,13 +13,9 @@ use Misaf\VendraTenant\Models\TenantDomain;
 /**
  * @extends Factory<TenantDomain>
  */
+#[UseModel(TenantDomain::class)]
 final class TenantDomainFactory extends Factory
 {
-    /**
-     * @var class-string<TenantDomain>
-     */
-    protected $model = TenantDomain::class;
-
     /**
      * @return array<string, mixed>
      */
