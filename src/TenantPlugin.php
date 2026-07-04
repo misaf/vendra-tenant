532c3c7d33f0c8be8181e3301c331789a9943c32
Misaf <misaf.1990@gmail.com>
2026-07-05 00:37:17 +0330

    Configure tenant panel branding

--- src/TenantPlugin.php
+++ src/TenantPlugin.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace Misaf\VendraTenant;
 
 use Filament\Contracts\Plugin;
+use Filament\Enums\ThemeMode;
+use Filament\FontProviders\LocalFontProvider;
 use Filament\Panel;
+use Filament\Support\Colors\Color;
+use Filament\Support\Enums\Width;
+use Misaf\VendraTenant\Models\Tenant;
 
 final class TenantPlugin implements Plugin
 {
@@ -29,7 +34,23 @@ final class TenantPlugin implements Plugin
         $panel->discoverResources(
             in: __DIR__ . '/Filament/Resources',
             for: 'Misaf\\VendraTenant\\Filament\\Resources',
-        );
+        )
+        // ->brandLogo(function () {
+        //     $tenant = Tenant::current();
+
+        //     return app()->environment('production') && $tenant
+        //         ? asset('images/' . $tenant->slug . '.webp')
+        //         : null;
+        // })
+            ->brandLogoHeight('10rem')
+            ->defaultThemeMode(ThemeMode::Dark)
+            ->font('yekan', 'https://cdn.font-store.ir/yekan.css', LocalFontProvider::class)
+            ->maxContentWidth(Width::Full)
+            ->tenant(Tenant::class)
+            // ->brandName(fn(GeneralSettings $generalSettings) => $generalSettings?->site_title ?? 'Default')
+            ->colors([
+                'primary' => Color::Gray
+            ]);
     }
 
     public function boot(Panel $panel): void {}
